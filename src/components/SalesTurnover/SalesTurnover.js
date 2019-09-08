540b8479b2084040e05ea50b55fd9162720296b3
rimanirfan <irfanriman@gmail.com>
2019-09-08 15:20:11 +0700

    added sales turnover component

--- src/components/SalesTurnover/SalesTurnover.js
+++ src/components/SalesTurnover/SalesTurnover.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import './SalesTurnover.css';
+
+
 const SalesTurnover = () => {
     return (
-        <div>
-            SalesTurnover
+        <div className="sales_turnover">
+            <div className="flex">
+                <div className="flex_item_seven">Sales Turnover</div>
+                <div>3 dots</div>
+            </div>
+            <div className="flex">
+                <div className="details flex_item_seven">
+                    <div>Rp 3,600,000</div>
+                    <div>13.8% last period in products sold</div>
+                </div>
+                <div>Cart icon</div>
+            </div>
         </div>
     );
 };
