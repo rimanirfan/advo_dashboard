17b0c609269a2083afc523e9a3b8507d1db30a3c
rimanirfan <irfanriman@gmail.com>
2019-09-08 10:35:57 +0700

    add styling to navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import './Navbar.css';
+
 const Navbar = () => {
-    return <div>Navbar</div>;
+    return (
+        <div className="ui secondary menu">
+            <div className="header item">
+                Our Company
+            </div>
+            <div className="right menu">
+                <div className="">
+                    <div className="user_detail">Username</div>
+                    <div className="user_detail">Company Name</div>
+                </div>
+                <div className="item">Icon</div>
+                <div className="item">Logout</div>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
