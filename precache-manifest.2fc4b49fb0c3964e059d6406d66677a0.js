6f02f5fdfba764bf8dd4d43686cd0a4dba5ff755
rimanirfan <irfanriman@gmail.com>
2019-09-10 00:00:48 +0700

    Updates

---
+++ precache-manifest.2fc4b49fb0c3964e059d6406d66677a0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed5bcd158561befa2b3a0675eaaae70",
+    "url": "/advo_dashboard/index.html"
+  },
+  {
+    "revision": "7f995ebb43a3a62da7f6",
+    "url": "/advo_dashboard/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "f086689a4b20a3e00a2f",
+    "url": "/advo_dashboard/static/css/main.a9b4f41a.chunk.css"
+  },
+  {
+    "revision": "7f995ebb43a3a62da7f6",
+    "url": "/advo_dashboard/static/js/2.50461b32.chunk.js"
+  },
+  {
+    "revision": "f086689a4b20a3e00a2f",
+    "url": "/advo_dashboard/static/js/main.c594fcc3.chunk.js"
+  },
+  {
+    "revision": "8a39d714051d0a4eae74",
+    "url": "/advo_dashboard/static/js/runtime~main.954f25ba.js"
+  },
+  {
+    "revision": "08d5a9c7695eabff42a3ffa391625c1e",
+    "url": "/advo_dashboard/static/media/advo_logo.08d5a9c7.jpg"
+  },
+  {
+    "revision": "da5d8ae55fa005ddb2479351440adb79",
+    "url": "/advo_dashboard/static/media/cart.da5d8ae5.svg"
+  },
+  {
+    "revision": "9bb0ae89bf7927cb1c94705a53afd22c",
+    "url": "/advo_dashboard/static/media/dashboard_icon.9bb0ae89.svg"
+  },
+  {
+    "revision": "5fd5bbafd4161d4ab338512cc8c49de1",
+    "url": "/advo_dashboard/static/media/dots.5fd5bbaf.svg"
+  },
+  {
+    "revision": "75511e53450d5cfafa3d574416f1323b",
+    "url": "/advo_dashboard/static/media/minuman.75511e53.jpg"
+  }
+]);
